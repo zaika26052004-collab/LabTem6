e8da3df2190898cebb57d8e36988f9a723506140
AlexeyZaika <zaika2605.2004@gmail.com>
2026-03-11 04:57:42 +0300

    Пользователь 2

--- LabTem6/LabTem6.cpp
+++ LabTem6/LabTem6.cpp
@@ -1,9 +1,10 @@
 ﻿#include <iostream>
 
 using namespace std;
+
 int main() {
     setlocale(0, "");
-    cout << "Тема №6\n Пользователь 2";
+    cout << "Тема №6 \n Пользователь 2 ";
     return 0;
 }
 
